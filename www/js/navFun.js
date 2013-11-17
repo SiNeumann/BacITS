1825d7bee56155b5793610374302d9adea80368b
Sigi <sigineumann@gmx.de>
2013-11-17 21:03:57 +0100

    sunday tryoutswiComments

--- www/js/navFun.js
+++ www/js/navFun.js
@@ -5,6 +5,7 @@
 
 function displayMenu(txt)
 {
+	//hier hab ich ein bißchen gebastelt, kann weg
 	alert(txt);
 	xmlDoc=loadXMLString(txt);
 	var news= xmlDoc.getElementsByName('News');
@@ -24,6 +25,8 @@ function displayRealContent(txt)
 
 function loadXMLDoc(XMLname)
 {
+	//hier wird das XML geladen, gefunden auf Stackoverflow
+	//Als Request vorgesehen damit XML online geladen werden können
 	//http://stackoverflow.com/questions/12566809/parsing-xml-file-with-javascript
     var xmlDoc;
     if (window.XMLHttpRequest)
@@ -51,6 +54,7 @@ function loadXMLDoc(XMLname)
 function loadXMLString(txt)
 {
 	//http://www.w3schools.com/dom/dom_loadxmldoc.asp
+	//wird ersetzt durch loadXMLDoc
 if (window.DOMParser)
   {
   parser=new DOMParser();
@@ -66,20 +70,25 @@ return xmlDoc;
 }
 function AddContent(id)
 {
+	//nimmt ID eines Elements 
 	elem=document.getElementById(id);
+	//Rücksetzen des Inhalts soll anschließend geladen werden
 	elem.innerHTML="";
+	//Hoffentlich ist das XMLFile vorhanden, sonst ist die Seite leer
 	var xName="xmlfiles/"+id+".xml";
 	var xDoc=loadXMLDoc("xmlfiles/"+id+".xml");
+	//Message ist eine Einheit im XML mit Text und Headline
 	var messages=xDoc.getElementsByTagName("Message");
 	
-	
+	//Schleife anpassen nach länge von Messages
 	for(var i=1;i<7;i++)
 		{
 			//get latest Entries
 			var message=messages.item(i-1);
 			var header=message.getElementsByTagName("Headline");
-			var div=document.createElement("div");
+			var div=document.createElement("div");//Erzeuge ein div anschließen Klasse auf infoUnit setzen
 			div.setAttribute("class","infoUnit");
+			//XML Element werden hier ausgelesen...
 			var text=message.getElementsByTagName("Text");
 			var myH1 = document.createElement("h1");
 			var testTex= text.item(0).firstChild.data;
@@ -87,6 +96,8 @@ function AddContent(id)
 			var myText = document.createTextNode(headLine);
 			var imag=document.createElement("img");
 			var source='images/'+ id + i+'.jpg';
+			//...und anschließend in neue erzeugte Elemente gesetzt
+			//die Elemente brauchen dringend eine besseres CSS als aktuell die infoUnit
 			imag.setAttribute("src", source);
 			imag.setAttribute("onclick", "displayRealContent('"+source+"')");               
 			imag.setAttribute("id","clickPic");

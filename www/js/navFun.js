c6cfc1a7ccf6240ea0ac8e5418cd1d1beb029441
Sigi <sigineumann@gmx.de>
2013-11-24 17:09:25 +0100

    SwipeTest

--- www/js/navFun.js
+++ www/js/navFun.js
@@ -18,9 +18,59 @@ function displayMenu(txt)
 	
 }
 
+
+function testswipe()
+{
+	alert("worked");
+}
+
 function displayRealContent(txt)
 {
-	alert(txt);
+	var split=txt.split(" ");
+	var count=split[1];
+	var id=split[0];
+	var xName="xmlfiles/"+id+".xml";
+	var xDoc=loadXMLDoc("xmlfiles/"+id+".xml");
+	//Message ist eine Einheit im XML mit Text und Headline
+	var messages=xDoc.getElementsByTagName("Message");
+	var message=messages.item(id-1);
+	var header=message.getElementsByTagName("Headline");
+	var div=document.createElement("div");//Erzeuge ein div anschließen Klasse auf infoUnit setzen
+	div.setAttribute("class","showcontent");
+	var elem=document.getElementById("showIt");
+	if(elem.childElementCount>0)
+	{
+			var max=childElementCount;
+			for(var i=0;i<max;i++)
+				{
+					elem.removeChild(elem.childNodes[0]);
+				}
+	}
+
+	//XML Element werden hier ausgelesen...
+	var text=message.getElementsByTagName("Text");
+	var myH1 = document.createElement("h1");
+	var myInnerdiv=document.createElement("div");
+	var testTex= text.item(0).firstChild.data;
+	var headLine=header.item(0).firstChild.data;
+	var myHeadline = document.createTextNode(headLine);
+	var myText=document.createTextNode(testTex);
+	var imag=document.createElement("img");
+	var source='images/'+ id + count+'.jpg';
+	//...und anschließend in neue erzeugte Elemente gesetzt
+	//die Elemente brauchen dringend eine besseres CSS als aktuell die infoUnit
+	imag.setAttribute("src", source);
+	//imag.setAttribute("onclick", "displayRealContent('"+id+" "+i+"')");               
+	//imag.setAttribute("id","clickPic");
+	div=setAttribute("swipeleft","testswipe()");
+	myH1.setAttribute("id","Headline");
+	myInnerdiv.appendChild(myText);
+	myH1.appendChild(myHeadline);
+	div.appendChild(myH1);
+	div.appendChild(imag);
+	div.appendChild(myText);
+	elem.appendChild(div);
+	
 }
 
 function loadXMLDoc(XMLname)
@@ -86,11 +136,13 @@ function AddContent(id)
 			//get latest Entries
 			var message=messages.item(i-1);
 			var header=message.getElementsByTagName("Headline");
-			var div=document.createElement("div");//Erzeuge ein div anschließen Klasse auf infoUnit setzen
+			var div=document.createElement("a");//Erzeuge ein div anschließen Klasse auf infoUnit setzen
 			div.setAttribute("class","infoUnit");
+			div.setAttribute("href","#showpage");
 			//XML Element werden hier ausgelesen...
 			var text=message.getElementsByTagName("Text");
 			var myH1 = document.createElement("h1");
+			
 			var testTex= text.item(0).firstChild.data;
 			var headLine=header.item(0).firstChild.data;
 			var myText = document.createTextNode(headLine);
@@ -99,7 +151,7 @@ function AddContent(id)
 			//...und anschließend in neue erzeugte Elemente gesetzt
 			//die Elemente brauchen dringend eine besseres CSS als aktuell die infoUnit
 			imag.setAttribute("src", source);
-			imag.setAttribute("onclick", "displayRealContent('"+source+"')");               
+			imag.setAttribute("onclick", "displayRealContent('"+id+" "+i+"')");               
 			imag.setAttribute("id","clickPic");
 			myH1.setAttribute("id","Headline");
 			

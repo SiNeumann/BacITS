3c9be3ea24968b687fd1126ae2960e0cf9032db3
agruenitsb <agruen.its-b2011@fh-salzburg.ac.at>
2013-12-29 17:00:11 +0100

    camera - second try

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+	$('#reader').html5_qrcode(function(data){
+			$('#read').html(data);
+		},
+		function(error){
+			$('#read_error').html(error);
+		}, function(videoError){
+			$('#vid_error').html(videoError);
+		}
+	);
+});
+
+
 function takePic(){
       navigator.camera.getPicture( function( imageURI ) {
         alert( imageURI );
